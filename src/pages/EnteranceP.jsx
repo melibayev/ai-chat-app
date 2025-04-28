2ed57ab8f8aa04b4b285d2bb3a04e1a428c4f4f3
melibayev <melibayefff1@gmail.com>
2025-04-29 01:30:19 +0500

    navigation

--- src/pages/EnteranceP.jsx
+++ src/pages/EnteranceP.jsx
@@ -4,7 +4,7 @@ import LOGO from '../assets/videos/dw.mp4';
 import styles from '../scss/Enterance.module.scss';
 
 // Preload HomeP
-const preloadHome = () => import('./HomeP');
+// const preloadHome = () => import('./HomeP');
 
 const EnteranceP = () => {
   const navigate = useNavigate();
@@ -12,15 +12,15 @@ const EnteranceP = () => {
 
   useEffect(() => {
     // Preload HomeP immediately
-    preloadHome();
+    // preloadHome();  
 
     // Stop the video and navigate after 4 seconds
     const timer = setTimeout(() => {
       if (videoRef.current) {
         videoRef.current.pause();
-        videoRef.current.currentTime = 0; // Optional: reset
+        videoRef.current.currentTime = 0; 
       }
-      navigate('/home', { replace: true }); // No back navigation
+      navigate('/home', { replace: true }); 
     }, 4000);
 
     // Block back button
